3a8ce7c7b4f4fc7d5fc03a518968d7b0d4a5eecf
CristianEduardMihai <cristianeduardmihai@gmail.com>
2024-02-13 20:24:28 +0200

    Added the ability to cancel a remote shutdown

--- windows-binary/src/main.rs
+++ windows-binary/src/main.rs
@@ -1,28 +1,59 @@
-#![windows_subsystem = "windows"]
+//#![windows_subsystem = "windows"]
 use std::net::UdpSocket;
-use std::str;
-use std::thread;
 use std::time::Duration;
+use std::thread;
+use std::io;
 use std::process::Command;
 
-fn main() {
+extern crate winapi;
+use winapi::um::winuser::{MessageBoxA, MB_OKCANCEL, MB_ICONEXCLAMATION, HWND_DESKTOP};
+
+fn main() -> io::Result<()> {
+    // Setting up UDP socket
     let socket = UdpSocket::bind("0.0.0.0:50000").expect("Could not bind socket");
-    let mut buf = [0u8; 25];
+    socket.set_nonblocking(true)?;
 
+    // Listening for UDP packets
+    let mut buf = [0; 25];
     loop {
-        let (received, _) = socket.recv_from(&mut buf).expect("Could not receive data");
-
-        let message = str::from_utf8(&buf[..received]).expect("Could not convert to string");
-
-        if message == "poweroff-securitykey" {
-            Command::new("shutdown")
-                .arg("-s")
-                .spawn()
-                .expect("Could not initiate shutdown");
-            std::process::exit(0);
+        match socket.recv_from(&mut buf) {
+            Ok((size, _)) => {
+                let received = String::from_utf8_lossy(&buf[..size]);
+                if received.trim() == "poweroff-securitykey" {
+                    show_popup()?;
+                }
+            },
+            Err(_) => (),
         }
-
-        thread::sleep(Duration::from_secs(5));
+        thread::sleep(Duration::from_millis(100));
     }
 }
+fn show_popup() -> io::Result<()> {
+    Command::new("shutdown")
+    .arg("/s")
+    .arg("/t")
+    .arg("10")
+    .arg("/c")
+    .arg(" ")
+    .spawn()
+    .expect("Could not initiate shutdown");    
 
+    println!("Shutting down in 10 seconds");
+
+    let message = "Your computer will shut down in 10 seconds. Press OK to proceed or Cancel to abort.";
+    let message_ansi = std::ffi::CString::new(message).expect("CString::new failed");
+    unsafe {
+        
+        let result = MessageBoxA(HWND_DESKTOP, message_ansi.as_ptr(), message_ansi.as_ptr(), MB_OKCANCEL | MB_ICONEXCLAMATION);
+        
+        if result == 2 {
+            println!("Shutdown cancelled.");
+            Command::new("shutdown")
+            .arg("/a")
+            .spawn()
+            .expect("Could not cancel shutdown");
+            return Ok(());
+        }
+    }
+    Ok(())
+}
